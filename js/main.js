8d104b1d6cbf45f35178c1d3dad632c4fc8a0d08
Cristian Camargo <chris.camargo2015@gmail.com>
2022-04-01 15:22:12 -0400

    criando banner e corrigindo cores dos links de navegacao

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+/* abrir e fechar o menu */
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for(const element of toggle){
+        element.addEventListener('click', () => {
+        nav.classList.toggle('show');
+    })
+}
+
+/* esconder menu ao clicar nos links */
+
+const links = document.querySelectorAll('nav ul li a');
+
+for(const link of links){
+    link.addEventListener('click', () => {
+    nav.classList.remove('show');
+    })
+};
+
+
+// mudar o header da pagina quando der scrool
+
+
+
+
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+function changeHeaderShadowOnScroll() {
+
+    if (window.scrollY >= navHeight){
+        // scroll e maior q a altura do header
+        header.classList.add('scroll')
+    } else {
+        // scroll e menor q a altura do header
+        header.classList.remove('scroll')
+
+    }
+}
