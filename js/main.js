b1a0a0c80cdbbcaa9c849c88b7702c85ace332f5
Cristian Camargo <chris.camargo2015@gmail.com>
2022-04-05 10:35:18 -0300

    criando swipper e adicionando responsividade, e corrigindo bugs

--- js/main.js
+++ js/main.js
@@ -38,3 +38,20 @@ function changeHeaderShadowOnScroll() {
 
     }
 }
+
+
+
+// Initializer Swipper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
